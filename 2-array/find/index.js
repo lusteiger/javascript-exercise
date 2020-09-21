908b509e6b6a0b68d7f100dca339623e0d961d1a
lusteiger <leonardoxy7@gmail.com>
2020-09-21 18:37:19 +0800

    [xinyu lu] implement the function

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,7 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  const found = collection.find(person => {
+    return person.age <= 20 && person.age >= 10;
+  });
+  return found.name;
 }
