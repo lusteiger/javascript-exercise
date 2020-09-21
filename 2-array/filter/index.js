df8f56d78e5208f62cf1e12b257fd331f8a387e9
lusteiger <leonardoxy7@gmail.com>
2020-09-21 18:30:58 +0800

    [xinyu lu] implement the funciton

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,16 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  const result = collection.filter(Number => {
+    return Number % 3 === 0;
+  });
+  return result;
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  const result = collection.filter((word, index) => {
+    return collection.indexOf(word) === index;
+  });
+  return result;
 }
-
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
