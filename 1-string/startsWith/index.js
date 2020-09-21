087fdbd4fad156142acee6402bf7604a9916b8c8
lusteiger <leonardoxy7@gmail.com>
2020-09-21 17:41:20 +0800

    [xinyu lu] implement the function

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,9 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+
+  let count = 0;
+  for (const i in collection) {
+    if (collection[i].startsWith('粤A')) count++;
+  }
+  return count;
 }
