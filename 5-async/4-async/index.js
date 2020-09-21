02ff515773d676ce4d7e21a1cd85c0ac9720dc33
lusteiger <leonardoxy7@gmail.com>
2020-09-21 23:29:00 +0800

    [xinyu lu] implement async/await function

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -8,4 +8,10 @@ async function fetchData(url) {
 }
 
 const URL = 'http://localhost:3000/api';
-fetchData(URL);
+fetchData(URL)
+  .then(result => {
+    document.writeln(result.name);
+  })
+  .catch(error => {
+    console.error(error);
+  });
